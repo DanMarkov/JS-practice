bf009df37417eb506c98934495e4fd24894467f6
Daniil Markov <danmark@ctemplar.com>
2022-05-04 15:42:52 +0800

    incrementations

---
+++ incrementFirstTemperature/index.js
@@ -0,0 +1,7 @@
+function incrementFirstTemperature(temperatures) {
+    return temperatures[0] + 1
+  }
+  
+  // Sample usage
+  console.log(incrementFirstTemperature([3, 5, 4, 7]))
+  console.log(incrementFirstTemperature([-5, 12, 15, 18, 13]))
