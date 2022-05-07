9b3b32f269512f7a3bd867e4367a9d66cf8acf75
Daniil Markov <danmark@ctemplar.com>
2022-05-07 18:51:26 +0800

    Arrays practice

--- Arrays/index.js
+++ Arrays/index.js
@@ -1,3 +1,11 @@
-let numbers = [3, 1, 6]
-let string = numbers.join(",") // "3;1;6"
-console.log(string)
+let grades = [10, 40, 9, 4, 1,]
+
+let sum = 0
+
+grades.forEach(function(grade){
+    if (grade > 10) {
+        sum = sum + grade
+    }
+})
+
+console.log(sum)
