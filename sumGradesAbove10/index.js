68673ae3251f8c033631e5c996a2590af28fb594
Daniil Markov <danmark@ctemplar.com>
2022-05-07 18:52:18 +0800

    Added some logic to arrays

---
+++ sumGradesAbove10/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {array} grades
+ */
+ function sumGradesAbove10(grades) {
+	let sum = 0
+    grades.forEach(function awesomeTool(grade){
+        if (grade >= 10) {
+            sum = sum + grade
+        }
+    })
+    return sum
+}
+
+// Sample usage - do not modify
+console.log(sumGradesAbove10([10, 5, 18, 3, 14, 19, 9])) // 61
+console.log(sumGradesAbove10([18, 4, 9, 20, 8])) // 38
