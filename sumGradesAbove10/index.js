9c0ac8a5b4bfc4ed68627bb2d086459f886bd44c
Daniil Markov <danmark@ctemplar.com>
2022-05-11 14:18:04 +0800

    Update index.js

--- sumGradesAbove10/index.js
+++ sumGradesAbove10/index.js
@@ -12,5 +12,5 @@
 }
 
 // Sample usage - do not modify
-console.log(sumGradesAbove10([10, 5, 18, 3, 14, 19, 9])) // 61
-console.log(sumGradesAbove10([18, 4, 9, 20, 8])) // 38
+console.log(sumGradesAbove10([10, 5, 18, 3, 14, 19])) // 61
+console.log(sumGradesAbove10([18, 4, 9, 20, 8, 45])) // 38
