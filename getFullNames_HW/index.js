f4b6b6d1b6d71e6b4e41338e52dec4191cbf3b32
Daniil Markov <alisasuperdetka.kqecn@dralias.com>
2022-05-20 10:32:12 +0800

    HOMEWORK!

---
+++ getFullNames_HW/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {{firstName: String, lastName: String, age: Number}[]} users
+ */
+ function getFullNames(users) {
+	return users.map(function(user){
+        return `${user.firstName} ${user.lastName}`;
+    }).join(", ");
+}
+
+// Sample usage - do not modify
+let users = [
+  {
+    firstName: "Sam",
+    lastName: "Blue",
+    age: 21,
+  },
+  {
+    firstName: "Charlie",
+    lastName: "Bon",
+    age: 38,
+  }
+];
+console.log(getFullNames(users)); // [Sam Blue, Charlie Bon]
