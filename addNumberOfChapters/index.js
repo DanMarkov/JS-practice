932be83e1ca047cc960fb86c48e74e31e5f7b3ad
Daniil Markov <alisasuperdetka.kqecn@dralias.com>
2022-05-20 10:29:00 +0800

    Practice with objects

---
+++ addNumberOfChapters/index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Object[]} course
+ * @param {number} count
+ */
+ function addNumberOfChapters(course, count) {
+    course.numberOfChapters = count;
+    console.log(course);
+    return course;
+  }
+  
+  // Sample usage - do not modify
+  let course1 = {
+    name: "Learn English",
+    isCompleted: true
+  };
+  let course2 = {
+    name: "Learn JavaScript",
+    isCompleted: false
+  };
+  
+  addNumberOfChapters(course1, 80); // { name: 'Learn English', isCompleted: true, numberOfChapters: 80 }
+  addNumberOfChapters(course2, 60); // { name: 'Learn JavaScript', isCompleted: false, numberOfChapters: 60 }
